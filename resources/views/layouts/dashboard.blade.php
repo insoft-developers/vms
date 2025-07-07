bb869fcac0deb54d1c470de599fb9eca4e2c94cf
Insoft Developers <irdn.software@gmail.com>
2025-07-07 12:31:01 +0700

    update title

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>VMS</title>
+    <title>VMS-ADMIN PANEL</title>
 
     @include('partials._head')
 </head>
