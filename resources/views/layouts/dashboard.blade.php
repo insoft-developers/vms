59bf47bdf5a9c44ddda68f8ed088a160ee6ed400
Insoft Developers <irdn.software@gmail.com>
2025-07-07 11:01:03 +0700

    test6

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>INSOFT CICD | Responsive Bootstrap 4 Admin Dashboard Template</title>
+    <title>INSOFT3 | Responsive Bootstrap 4 Admin Dashboard Template</title>
 
     @include('partials._head')
 </head>
