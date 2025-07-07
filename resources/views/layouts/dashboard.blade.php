fb16c0091fb32c2709bf2b3887cb8439e42486c2
Insoft Developers <irdn.software@gmail.com>
2025-07-07 12:48:39 +0700

    perbaiki ht asccess

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>VMS-ADMIN PANEL</title>
+    <title>VMS2-ADMIN PANEL</title>
 
     @include('partials._head')
 </head>
