eb2072c470962927697e4daca584add211ed725f
Insoft Developers <irdn.software@gmail.com>
2025-07-07 10:50:11 +0700

    test 3

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>GIG CICD | Responsive Bootstrap 4 Admin Dashboard Template</title>
+    <title>GIGE CICD | Responsive Bootstrap 4 Admin Dashboard Template</title>
 
     @include('partials._head')
 </head>
