cc4b85ccbb2a37208286272699b51d1e7067df4e
Insoft Developers <irdn.software@gmail.com>
2025-07-07 10:36:17 +0700

    action git

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Glare CICD | Responsive Bootstrap 4 Admin Dashboard Template</title>
+    <title>GIG CICD | Responsive Bootstrap 4 Admin Dashboard Template</title>
 
     @include('partials._head')
 </head>
